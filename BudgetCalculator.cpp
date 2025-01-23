539755a749927c5a191efee68fb16387b2217e48
Nathan Bartholomew <69134168+nki3@users.noreply.github.com>
2025-01-23 17:16:09 -0500

    Added user input for costs, tickets, and donations

--- BudgetCalculator.cpp
+++ BudgetCalculator.cpp
@@ -5,8 +5,26 @@
 #include "BudgetCalculator.h"
 
 #include <iostream>
+#include <iomanip>
 
 int main() {
-    std::cout << "Hello World!";
+    int venue_cost, food_cost, entertainment_cost, tickets_sold, donations;
+
+    const int ticket_price = 10;
+
+    std::cout << "Enter the venue cost: ";
+    std::cin >> venue_cost;
+    std::cout << "Enter the food cost: ";
+    std::cin >> food_cost;
+    std::cout << "Enter the entertainment cost: ";
+    std::cin >> entertainment_cost;
+    std::cout << "Enter the number of tickets sold: ";
+    std::cin >> tickets_sold;
+    std::cout << "Enter the amount of donations: ";
+    std::cin >> donations;
+
+    int total_cost = venue_cost + food_cost +entertainment_cost;
+    int total_income = tickets_sold*ticket_price + donations;
+
     return 0;
 }
