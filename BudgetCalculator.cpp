e6f60ef5a248f38adda78a517f5e338877ebfce3
Nathan Bartholomew <69134168+nki3@users.noreply.github.com>
2025-01-23 15:51:19 -0500

    Initial commit

---
+++ BudgetCalculator.cpp
@@ -0,0 +1,12 @@
+//
+// Created by NateB on 1/23/2025.
+//
+
+#include "BudgetCalculator.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
---
+++ BudgetCalculator.h
@@ -0,0 +1,16 @@
+//
+// Created by NateB on 1/23/2025.
+//
+
+#ifndef BUDGETCALCULATOR_H
+#define BUDGETCALCULATOR_H
+
+
+
+class BudgetCalculator {
+
+};
+
+
+
+#endif //BUDGETCALCULATOR_H
