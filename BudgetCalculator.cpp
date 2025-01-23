0a571103a085d5bf75707aa8b0e75243bb8e13b7
Nathan Bartholomew <69134168+nki3@users.noreply.github.com>
2025-01-23 18:21:53 -0500

    Added output tables

--- BudgetCalculator.cpp
+++ BudgetCalculator.cpp
@@ -6,6 +6,27 @@
 
 #include <iostream>
 #include <iomanip>
+#include <utility>
+
+void print_row(int name_width, int value_width, std::string name, std::string value) {
+    std::cout << std::left << std::setw(name_width) << name
+        << std::right << std::setw(value_width) << value << std::endl;
+}
+
+void print_table(std::string table_name, std::string column_name, std::pair<std::string, int> rows[], int row_length) {
+
+    const int name_width = 18;
+    const int value_width = 7;
+
+    print_row(name_width, value_width, table_name, column_name);
+    print_row(name_width, value_width, "------------", "-------");
+    for (int i = 0; i < row_length-1; i++) {
+        print_row(name_width, value_width, rows[i].first, std::to_string(rows[i].second));
+    }
+    print_row(name_width, value_width, "------------", "-------");
+    print_row(name_width, value_width, rows[row_length-1].first, std::to_string(rows[row_length-1].second));
+}
+
 
 int main() {
     int venue_cost, food_cost, entertainment_cost, tickets_sold, donations;
@@ -26,5 +47,23 @@ int main() {
     int total_cost = venue_cost + food_cost +entertainment_cost;
     int total_income = tickets_sold*ticket_price + donations;
 
+    std::pair<std::string, int> costs[] = {
+        {"Venue", venue_cost},
+        {"Food", food_cost},
+        {"Entertainment", entertainment_cost},
+        {"Total Cost", total_cost}
+    };
+
+    std::pair<std::string, int> income[] = {
+        {"Tickets", tickets_sold},
+        {"Donation", donations},
+        {"Total", total_income}
+    };
+
+    print_table("Costs", "Amount", costs, 4);
+    std::cout << std::endl;
+    print_table("Income", "Amount", income, 3);
+
+    std::cout << std::endl << "Total Raised for Spud4Kids: $" << total_income - total_cost << std::endl;
     return 0;
 }
