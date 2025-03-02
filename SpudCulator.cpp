fecf063e97f67ec9b9354beca8f5ae326e08d012
Nathan Bartholomew <69134168+nki3@users.noreply.github.com>
2025-03-02 00:26:50 -0500

    Added SpudCulator

---
+++ SpudCulator.cpp
@@ -0,0 +1,18 @@
+//
+// Created by NateB on 3/1/2025.
+//
+
+#include <iostream>
+
+void displayMessage(int count) {
+
+}
+
+double calculateTotalValue(double weight, double pricePerPound) {
+    return 0;
+}
+
+int main() {
+    std::cout << "Welcome to the SpudCulator!" << std::endl;
+    return 0;
+}
